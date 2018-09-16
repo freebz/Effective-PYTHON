229b95d8d50ed5c853c6c4e146dcce7cd2fcde00
FreeBz <freebz@hananet.net>
2018-09-16 21:53:08 +0900

    Better way 50 모듈을 구성하고 안정적인 API를 제공하려면 패키지를 사용하자

---
+++ ch07/better_way_50/ex50-3.main2.py
@@ -0,0 +1,3 @@
+# main2.py
+from analysis.utils import inspect
+from frontend.utils import inspect  # 덮어씀!
---
+++ ch07/better_way_50/mypackage/utils.py
@@ -0,0 +1,10 @@
+# utils.py
+from . models import Projectile
+
+__all__ = ['simulate_collision']
+
+def _dot_product(a, b):
+    # ...
+
+def simulate_collision(a, b):
+    # ...
